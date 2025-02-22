3e6b4bd21f4b42e27a84323132267a29c7d0af48
senbagaraman <senbagaraman7330@gmail.com>
2025-02-22 18:56:33 +0530

    Session work

---
+++ session_class.php
@@ -0,0 +1,82 @@
+<?PHP 
+
+
+class Session {
+
+// Session start function 
+public static function start(){
+    session_start();
+}
+
+// session unset function 
+public static function unset(){
+    session_unset();
+}
+
+// Session destory function 
+public static function destory(){
+    session_destroy();
+
+}
+
+// assign the values in the form key value pair
+public  static function set($key , $value){
+    $_SESSION[$key] = $value;
+}
+
+// del the particular item in session
+public static function del($key){
+       unset($_SESSION[$key]);
+}
+
+// Check the value is existing 
+public static function isset($key){
+    isset($_SESSION[$key]);
+}
+
+// get the value in session 
+public static function get($key, $default = false){
+    if (Session::isset($_SESSION[$key])){
+        return $_SESSION[$key];
+    }
+    else{
+          return $default;   // if it's not default value is return.
+    }
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
